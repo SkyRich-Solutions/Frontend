9cf17b8a9fb4625d53ff784982c0edb91d6a9097
Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
2025-02-12 21:21:44 +0100

    Revert "Base layout router"

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Outlet } from 'react-router';
-
-import Sidebar from './Components/SideBar';
-
 function App() {
-
     return (
-        <div className="flex h-dvh w-full bg-gray-900 text-gray-100 overflow-hidden">
-            <div className='fixed inset-0 z-0'>
-                <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80'/>
-                <div className=' absolute inset-0 backdrop-blur-sm'/>
-            </div>
-            <Sidebar/>
-           
-                <Outlet/>
-            
+        <div className='App'>
+            <header className='App-header'>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Learn React
+                </a>
+            </header>
         </div>
     );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,19 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider, createHashRouter } from "react-router";
 import App from './App';
-import UploadPage from './Pages/Upload';
-
-
-
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/upload",
-        element: <UploadPage/>,
-      },
-    ],
-  },
-]);
+import './index.css';
+import JSONTable from './Pages/Test';
+import Test2 from './Pages/Test2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <Test2 /> */}
+
+        <JSONTable />
+    </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
