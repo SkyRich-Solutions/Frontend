bb8ef0e3f68ac955e0a6b93a092329099a738c8e
Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
2025-02-12 21:30:32 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createHashRouter } from "react-router";
+import App from './App';
+import UploadPage from './Pages/Upload';
 
+
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/upload",
+        element: <UploadPage/>,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
