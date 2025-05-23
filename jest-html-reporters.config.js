df17b6d1960a6f2b0dbf6fe1a845bf7fbb883e50
Andreea101018 <andreea.asimine@yahoo.com>
2025-05-23 12:33:40 +0200

    Changing pipeline

--- jest-html-reporters.config.js
+++ jest-html-reporters.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
   publicPath: "./",
   filename: "test-report.html",
   expand: true,
-  pageTitle: "Frontend Unit Test Report"
+  pageTitle: "Frontend Unit Test Report",
+  outputPath: path.resolve(__dirname)
 };
